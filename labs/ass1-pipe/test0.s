319c8751f9139ef33e40e695d4808fb031272b78
Philip Leong <philip.leong@sydney.edu.au>
2023-09-02 20:00:22 +1000

    update ass

---
+++ labs/ass1-pipe/test0.s
@@ -0,0 +1,32 @@
+/*
+ *  NERV -- Naive Educational RISC-V Processor
+ *
+ *  Copyright (C) 2020  Claire Xenia Wolf <claire@yosyshq.com>
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+.section .text
+.global main
+.global _start
+_start:
+    addi x2, zero, 0
+    addi x1, zero, 0
+    addi x3, zero, 10
+loop:   
+    add x1, x1, x2
+    ble x2, x3, loop
+    addi x2, x2, 1
+    addi x10,x1,0
+    ebreak
